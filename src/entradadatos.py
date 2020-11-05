5a94569bdaf68f3a73cb77a2869512cfbc62774e
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-06 00:18:05 +0100

    Update entradadatos.py

--- src/entradadatos.py
+++ src/entradadatos.py
@@ -3,3 +3,12 @@
 mail = input("Introduce tu dirección de email")
 
 print("Tu dirección de correo es: " + mail)
+
+"""
+Los datos almacenados por el método input son stings.
+Para indicarle al programa que la variable a introducir es un número.
+"""
+
+edad = int(input("¿Cuántos años tienes?"))
+
+print(edad)
