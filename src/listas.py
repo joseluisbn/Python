5ee18a85317126028067dbca1b216073ca7bb72b
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-06 00:39:55 +0100

    Create listas.py

---
+++ src/listas.py
@@ -0,0 +1,9 @@
+# Listas
+
+pandilla = ["Dani", "Yajie", "José Luis"]
+
+print(pandilla)
+
+# Para imprimir solo una parte de la lista, nos remitiremos a su lugar dentro del índice
+
+print(pandilla[1])
